aa1389f8c265d6729154609bed86ebd814592b9a
liufan <liufan94@163.com>
2023-07-13 14:28:29 +0800

    11

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -11,6 +11,7 @@ export default defineComponent({
         home
         <lf-button>Primary Button</lf-button>
         <aa-button type="primary">gggg</aa-button>
+        <aa-button type="primary">男男女女</aa-button>
       </div>
     );
   },
